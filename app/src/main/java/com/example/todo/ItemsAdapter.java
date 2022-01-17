71fd19f3ec8eaba1f8879a34acdd29d390fdc99e
Mason Woodford <mwoodfor@ucsc.edu>
2022-01-17 01:32:03 -0800

    added list item borders

--- app/src/main/java/com/example/todo/ItemsAdapter.java
+++ app/src/main/java/com/example/todo/ItemsAdapter.java
@@ -1,6 +1,5 @@
 package com.example.todo;
 
-import android.graphics.drawable.LayerDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +7,6 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.helper.widget.Layer;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
